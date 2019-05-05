9287f9310ca5854d14b9bfc68171e2f48216385d
Administrator <Administrator@192.168.1.4>
2019-05-06 00:15:22 +0800

    二叉查找树

---
+++ src/org/chapter25/base/Tree.java
@@ -0,0 +1,24 @@
+package org.chapter25.base;
+
+public interface Tree<E> extends Iterable<E> {
+	
+	public boolean search(E e);
+	
+	public boolean insert(E e);
+	
+	public boolean delete(E e);
+	
+	//中序
+	public void inorder();
+	
+	//后序
+	public void postorder();
+	
+	//前序
+	public void preeorder();
+	
+	public int getSize();
+	
+	public boolean isEmpty();
+	
+}
---
+++ src/org/chapter25/base/TreeNode.java
@@ -0,0 +1,12 @@
+package org.chapter25.base;
+
+public class TreeNode<E> {
+	protected E element;
+	protected TreeNode<E> left;
+	protected TreeNode<E> right;
+	
+	public TreeNode(E element) {
+		this.element = element;
+	}
+	
+}
