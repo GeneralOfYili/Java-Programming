59af1a7fca13a149bd36d19758e00cf7cbb8079b
GeneralOfYili <johnzhengnan@163.com>
2019-05-06 17:11:19 +0800

    抽象树

---
+++ src/org/chapter25/base/AbstractTree.java
@@ -0,0 +1,29 @@
+package org.chapter25.base;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+
+	@Override
+	public void inorder() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postorder() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preeorder() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return getSize() == 0;
+	}
+
+}
